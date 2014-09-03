4185d591f589b5b2cc9eb62c68e211933281d928
Oscar Maldonado <oscar.maldonado@isolution.mx>
2014-09-02 19:37:26 -0500

    update de links

--- pruebas/docview/pdfjs_test.php
+++ pruebas/docview/pdfjs_test.php
@@ -1,5 +1,8 @@
 <?php 
-$file = htmlentities("http://localhost/doc.pdf");
+// $file = "http://192.168.231.4/pae/pruebas/docview/doc.pdf";
+// $page = 'http://192.168.231.4/pae/pruebas/docview/pdfjs/viewpdf.php?archivo='.$file;
+
+$file = "http://localhost/pae/pruebas/docview/doc.pdf";
 $page = 'http://localhost/pae/pruebas/docview/pdfjs/viewpdf.php?archivo='.$file;
 
 ?>
